b5afe74c95df155aaa50dec81e84c149958ca43b
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-01-21 21:23:19 -0800

    Added scripts

---
+++ Assets/_Project/Scripts/ScreenMainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenMainMenu : MonoBehaviour
+{
+    [SerializeField] Button btnPlay;
+    [SerializeField] Button btnLoad;
+    [SerializeField] Button btnSettings;
+    [SerializeField] Button btnAbout;
+    [SerializeField] Button btnExit;
+
+    void Start()
+    {
+
+    }
+}
